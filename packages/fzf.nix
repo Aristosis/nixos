25c2823e48a76e4949aefda449f911f89d4693ab
ari <communeofstars@proton.me>
2026-09-18 18:28:44 +0530

    fzf coloaz

--- packages/fzf.nix
+++ packages/fzf.nix
@@ -9,7 +9,15 @@ symlinkJoin {
   nativeBuildInputs = [ makeWrapper ];
   postBuild = ''
     wrapProgram $out/bin/fzf \
-      --add-flags "--color=bg:-1,bg+:-1,gutter:-1"
+      --add-flags "--color=bg:-1" \
+      --add-flags "--color=bg+:-1" \
+      --add-flags "--color=pointer:#ffc0cb" \
+      --add-flags "--color=marker:#ffa500" \
+      --add-flags "--color=hl+:#00ffff" \
+      --add-flags "--color=hl:#00ffff" \
+      --add-flags "--color=info:#b2b2b2" \
+      --add-flags "--color=prompt:#ff0000" \
+      --add-flags "--gutter=\" \""
   '';
   meta.mainProgram = "fzf";
 }
